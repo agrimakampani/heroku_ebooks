31cc802fb710648d8c145cf28ddb13cd16ab6b70
agrimakampani <60799380+agrimakampani@users.noreply.github.com>
2020-09-05 07:46:21 +0000

    Update local_settings.py

--- local_settings.py
+++ local_settings.py
@@ -7,10 +7,10 @@ Local Settings for a heroku_ebooks account.
 # Configuration for Twitter API
 ENABLE_TWITTER_SOURCES = True # Fetch twitter statuses as source
 ENABLE_TWITTER_POSTING = True # Tweet resulting status?
-MY_CONSUMER_KEY = environ.get('TWITTER_CONSUMER_KEY')#Your Twitter API Consumer Key set in Heroku config
-MY_CONSUMER_SECRET = environ.get('TWITTER_CONSUMER_SECRET')#Your Consumer Secret Key set in Heroku config
-MY_ACCESS_TOKEN_KEY = environ.get('TWITTER_ACCESS_TOKEN_KEY')#Your Twitter API Access Token Key set in Heroku config
-MY_ACCESS_TOKEN_SECRET = environ.get('TWITTER_ACCESS_SECRET')#Your Access Token Secret set in Heroku config
+MY_CONSUMER_KEY = environ.get('WRXJA3SVRE36PKYAiRdl6oeVu')#Your Twitter API Consumer Key set in Heroku config
+MY_CONSUMER_SECRET = environ.get('z7LFxvuf8F0bu42JeQqQOrsvkcoLIwduZ6z9J2SbmG1PATV9A8')#Your Consumer Secret Key set in Heroku config
+MY_ACCESS_TOKEN_KEY = environ.get('1300337748482682883-Ova0fVkfcPOmIUQE48uUWbCHiIPNOx')#Your Twitter API Access Token Key set in Heroku config
+MY_ACCESS_TOKEN_SECRET = environ.get('zTmsk5SWGMKWIdlWim4pg3g34suGG1aT8WKMvsd0CJGvu')#Your Access Token Secret set in Heroku config
 
 # Configuration for Mastodon API
 ENABLE_MASTODON_SOURCES = False # Fetch mastodon statuses as a source?
@@ -20,7 +20,7 @@ CLIENT_CRED_FILENAME = '' # the MASTODON client secret file you created for this
 USER_ACCESS_FILENAME = '' # The MASTODON user credential file you created at installation.
 
 # Sources (Twitter, Mastodon, local text file or a web page)
-TWITTER_SOURCE_ACCOUNTS = [""]  # A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
+TWITTER_SOURCE_ACCOUNTS = ["GRE__Words", "GREwordlist"]  # A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
 MASTODON_SOURCE_ACCOUNTS = [""] # A list, e.g. ["@user@instance.tld"]
 SOURCE_EXCLUDE = r'^$'  # Source tweets that match this regexp will not be added to the Markov chain. You might want to filter out inappropriate words for example.
 STATIC_TEST = False  # Set this to True if you want to test Markov generation from a static file instead of the API.
@@ -33,8 +33,8 @@ WEB_ATTRIBUTES = [{'class': 'example-text'}, {}] # A list of dictionaries contai
 ODDS = 8  # How often do you want this to run? 1/8 times?
 ORDER = 2  # How closely do you want this to hew to sensical? 2 is low and 4 is high.
 
-DEBUG = True  # Set this to False to start Tweeting live
-TWEET_ACCOUNT = ""  # The name of the account you're tweeting to.
+DEBUG = False  # Set this to False to start Tweeting live
+TWEET_ACCOUNT = "GREBOT5"  # The name of the account you're tweeting to.
 
 #Configuration for Twitter parser. TEST_SOURCE will be re-used as as the corpus location.
 TWITTER_ARCHIVE_NAME = "tweets.csv" #Name of your twitter archive
